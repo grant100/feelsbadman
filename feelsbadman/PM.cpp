001ba60671bdfeaeab70fa6d54f289859c8ca0f0
grant100 <gsowards100@gmail.com>
2019-04-11 19:30:03 -0700

    process identification

--- feelsbadman/PM.cpp
+++ feelsbadman/PM.cpp
@@ -4,8 +4,52 @@
 #include <Windows.h>
 #include <Psapi.h>
 #include <iostream>
+#include <TlHelp32.h>
+
 bool PM::getProcesses() {
-	DWORD pids[1024], cb;
+	/*
+Function takes in a string value for the process it is looking for like ST3Monitor.exe
+then loops through all of the processes that are currently running on windows.
+If the process is found it is running, therefore the function returns true.
+*/
+	std::string process = "x64dbg.exe";
+	std::string compare;
+	bool procRunning = false;
+
+	HANDLE hProcessSnap;
+	PROCESSENTRY32 pe32;
+	hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+	if (hProcessSnap == INVALID_HANDLE_VALUE) {
+		procRunning = false;
+	}
+	else {
+		pe32.dwSize = sizeof(PROCESSENTRY32);
+		if (Process32First(hProcessSnap, &pe32)) { // Gets first running process
+			if (pe32.szExeFile == process) {
+				procRunning = true;
+			}
+			else {
+				// loop through all running processes looking for process
+				while (Process32Next(hProcessSnap, &pe32)) {
+					// Set to an AnsiString instead of Char[] to make compare easier
+					
+					compare = pe32.szExeFile;
+					
+					if (compare == process) {
+						// if found process is running, set to true and break from loop
+						procRunning = true;
+						break;
+					}
+				}
+			}
+			// clean the snapshot object
+			CloseHandle(hProcessSnap);
+		}
+	}
+
+	return procRunning;
+/*	DWORD pids[1024], cb;
 	EnumProcesses(pids, sizeof(pids), &cb);
 
 	DWORD count = cb / sizeof(DWORD);
@@ -25,7 +69,7 @@ bool PM::getProcesses() {
 			}
 		}
 	}
-	return FALSE;
+	return FALSE;*/
 }
 /*
 void PM::hasProcesses() {
